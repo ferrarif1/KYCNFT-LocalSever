d485f5323c748655ca3b673030802a5ff667c4bc
ferrarif1 <15858188446@163.com>
2022-03-03 09:46:11 +0800

    update

--- truffle-config.js
+++ truffle-config.js
@@ -42,7 +42,7 @@ module.exports = {
     //
     development: {
      host: "127.0.0.1",     // Localhost (default: none)
-     port: 8545,            // Standard Ethereum port (default: none)
+     port: 7545,            // Standard Ethereum port (default: none)
      network_id: "*",       // Any network (default: none)
     },
 
@@ -85,7 +85,7 @@ module.exports = {
   // Configure your compilers
   compilers: {
    solc: {
-        version: "^0.6.0",
+        version: "^0.8.0",
         settings: {
           optimizer: {
             enabled : true,
